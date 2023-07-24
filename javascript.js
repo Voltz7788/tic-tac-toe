8009aabfc58089f2b187f8a12a14fdd48ae4e8b3
Yannis <yannismadu777@gmail.com>
2023-07-24 19:25:43 +0100

    Add win conditions and reset game button

--- javascript.js
+++ javascript.js
@@ -3,7 +3,7 @@ const myGameboard = (() => {
     let gameboardArray = []
     let turn = true;
 
-    const createGridTile = () => {
+    const createGridTile = function () {
         let tileStatus = "";
 
         const setToX = function () {
@@ -19,54 +19,139 @@ const myGameboard = (() => {
             console.log(`Tile reset`)
         }
 
-        const getTileStatus = function() {
+        const getTileStatus = function () {
             return tileStatus;
         }
 
         return { setToX, setToO, resetTile, getTileStatus }
     }
 
-    const populateGameboard = () => {
+    const populateGameboard = function () {
         for (let i = 1; i <= 9; i++) {
             gameboardArray.push(createGridTile())
         };
     };
 
-    function addButtonToTile() {
+    const addButtonToTile = function () {
         for (let index in myGameboard.gameboardArray) {
             const tile = document.getElementById(`tile${index}`);
             tile.addEventListener("click", () => {
-                console.log(`tile${index}`);
-                console.log(turn);
-
                 if (myGameboard.gameboardArray[index].getTileStatus() === "") {
-                    markTile(`tile${index}`, index);
+                    trackMove(`tile${index}`, index);
+                    if (checkWin("x") || checkWin("o") || checkDraw()) {
+                        console.log("Game over")
+                        displayController.addPlayAgainButton();
+                    }
+                    // checkWin("x");
+                    // checkWin("o");
+                    // checkDraw();
                     swapTurn();
                 }
-                
+
             });
         };
     };
 
-    function swapTurn() {
+    
+
+
+    const checkWin = function (marker) {
+        let resultsArray = [];
+        for (let tile of myGameboard.gameboardArray) {
+            resultsArray.push(tile.getTileStatus())
+        }
+
+        // Check horizontal win
+        if ((resultsArray[0] == marker && resultsArray[1] == marker && resultsArray[2] == marker)
+            || (resultsArray[3] == marker && resultsArray[4] == marker && resultsArray[5] == marker)
+            || (resultsArray[6] == marker && resultsArray[7] == marker && resultsArray[8] == marker)) {
+            console.log("win");
+            return true;
+        } else if ((resultsArray[0] == marker && resultsArray[3] == marker && resultsArray[6] == marker)
+            || (resultsArray[1] == marker && resultsArray[4] == marker && resultsArray[7] == marker)
+            || (resultsArray[2] == marker && resultsArray[5] == marker && resultsArray[8] == marker)) {
+            console.log("win");
+            return true;
+        } else if ((resultsArray[0] == marker && resultsArray[4] == marker && resultsArray[8] == marker)
+            || (resultsArray[2] == marker && resultsArray[4] == marker && resultsArray[6] == marker)) {
+            console.log("win");
+            return true;
+        }
+
+    };
+
+    const checkDraw = function () {
+        let counter = 0
+        for (let tile of myGameboard.gameboardArray) {
+            if (tile.getTileStatus() != "") {
+                counter++
+            }
+        }
+
+        if (counter == 9) {
+            console.log("draw")
+            return true;
+        }
+    };
+
+    const swapTurn = function () {
         turn = !turn;
-    }
+    };
 
-    function markTile(id, index) {
-        const tile = document.getElementById(id)
-            if (turn) {
-                myGameboard.gameboardArray[index].setToX();
-                tile.textContent = "X";
-                console.log(myGameboard.gameboardArray[index].getTileStatus())
-                
-            } else {
-                myGameboard.gameboardArray[index].setToO();
-                tile.textContent = "O";
-                console.log(myGameboard.gameboardArray[index].getTileStatus())
-            }; 
-    }
+    const trackMove = function (id, index) {
+        if (turn) {
+            myGameboard.gameboardArray[index].setToX();
+            displayController.markAsX(id)
+            console.log(myGameboard.gameboardArray[index].getTileStatus())
 
-    return { populateGameboard, addButtonToTile, gameboardArray };
+        } else {
+            myGameboard.gameboardArray[index].setToO();
+            displayController.markAsO(id);
+            console.log(myGameboard.gameboardArray[index].getTileStatus())
+        };
+    };
+
+    const gameReset = function () {
+        for (let index in myGameboard.gameboardArray) {
+            displayController.resetTile(`tile${index}`);
+            myGameboard.gameboardArray[index].resetTile();
+
+        }
+    };
+
+    return { populateGameboard, addButtonToTile, gameReset, gameboardArray };
+})();
+
+const displayController = (() => {
+    const markAsX = function (id) {
+        const tile = document.getElementById(id);
+        tile.textContent = "X";
+    };
+
+    const markAsO = function(id) {
+        const tile = document.getElementById(id);
+        tile.textContent = "O";
+    };
+
+    const resetTile = function(id) {
+        const tile = document.getElementById(id);
+        tile.textContent = "";
+    };
+
+    const addPlayAgainButton = function() {
+        const gameContainer = document.querySelector(".gameContainer");
+        const button = document.createElement("button");
+        button.textContent = "Play again";
+
+        button.addEventListener("click", () => {
+            myGameboard.gameReset();
+            gameContainer.removeChild(button);
+        });
+
+        gameContainer.appendChild(button);
+    };
+
+    return { markAsX, markAsO, resetTile, addPlayAgainButton };
 })();
 
 const createPlayer = (name, marker) => {
@@ -78,6 +163,11 @@ const createPlayer = (name, marker) => {
 
 const player1 = createPlayer("Yannis", "x");
 const player2 = createPlayer("Andy", "o");
+
+
+
+
+
 
 
 
